6a7ef6a90c6e155a189623ecb314dc6bbeb0fcd7
sayandeepdas02 <das2002sayandeep@gmail.com>
2026-02-12 18:06:39 +0530

    chore(cleanup): remove unused proctoring routes

--- FluxAI-backend/src/modules/proctoring/proctoring.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { proctoringController } from './proctoring.controller.js'
-
-const router = Router()
-
-// Proctoring routes are nested under attempts
-// These will be merged into attempts routes
-
-export default router
